2199bd1102a955f18f870b7dc5616c2743b51c06
xResh <32607024+xResh@users.noreply.github.com>
2017-10-08 21:30:39 -0400

    Deleted commented out code

--- hw09proj/src/SongParserTest.java
+++ hw09proj/src/SongParserTest.java
@@ -7,12 +7,10 @@ import java.util.*;
 public class SongParserTest {
   
 	  @Test public void test1() throws IOException, SongParser.FormatException {
-	    // Here's how to test expecting an exception...
 	    try {
 	      new SongParser(null);
 	      fail("Expected an IllegalArgumentException - cannot create SongParser with null.");
 	    } catch (IllegalArgumentException f) {    
-	      //Do nothing. It's supposed to throw an exception
 	    }
 	  }
 	  
@@ -21,10 +19,8 @@ public class SongParserTest {
 		  List<StoredNote> q = c.getSheet();
 		    assertEquals("try1", q.get(0).getKey(), 'q');
 		    assertEquals("try2", q.get(0).getStartTime(), 0);
-		    //q.remove();
 		    assertEquals("try3", q.get(1).getKey(), 'o');
 		    assertEquals("try4", q.get(1).getStartTime(), 60);
-		    //q.remove();
 		    assertEquals("try5", q.get(2).getKey(), 'w');
 		    assertEquals("try6", q.get(2).getStartTime(), 100);
 		    
